4ab1e2622b730375747f1f8ea02db886d048f863
YulyKo <juliakotenko.forstudy@gmail.com>
2019-09-10 21:01:11 +0300

    task5 done

---
+++ task5.rb
@@ -0,0 +1,6 @@
+puts 'task 5'
+firstValue = 34
+secondValue = -344
+averageMean = (firstValue + secondValue)/2
+avaregeGeometricWithModule = Math.sqrt((firstValue**2).abs + (secondValue**2).abs)
+puts "Average Mean = #{averageMean}\nAvarege Geometric With Module = #{avaregeGeometricWithModule}"
