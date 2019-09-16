0f5ca266aa144efe223d24ddda76c75d0f2b2ffd
YulyKo <juliakotenko.forstudy@gmail.com>
2019-09-16 19:45:01 +0300

    task 23: calculating of medians was done

--- task23.rb
+++ task23.rb
@@ -1,6 +1,6 @@
-side_a = 2
-side_b = 76
-side_c = 83
+side_a = 4
+side_b = 5
+side_c = 6
 
 puts "a = #{side_a} || b = #{side_b} || c = #{side_c}"
 
@@ -13,3 +13,15 @@ h__from_b = calculate_h(side_b)
 h_from_c = calculate_h(side_c)
 
 puts "h from a = #{side_a} || h from b = #{side_b} || h from c = #{side_c}"
+
+def calculate_m(side_a, side_b, side_c)
+  m_a = 0.5 * Math.sqrt(2 * side_b**2 + (2 * side_c**2) - side_a**2)
+end
+
+median_of_a = calculate_m(side_a, side_b, side_c)
+median_of_b = calculate_m(side_b, side_c, side_a)
+median_of_c = calculate_m(side_c, side_b, side_a)
+
+puts "m of a = #{median_of_a} || m of b = #{median_of_b} || m of c = #{median_of_c}"
+
+
