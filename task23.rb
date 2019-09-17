5f891d841da3d67270b6ec2dce5fea5495740c00
YulyKo <juliakotenko.forstudy@gmail.com>
2019-09-17 09:04:12 +0300

    task 23: calculating of bisectors was done

--- task23.rb
+++ task23.rb
@@ -1,6 +1,6 @@
-side_a = 4
-side_b = 5
-side_c = 6
+side_a = 10
+side_b = 10
+side_c = 5
 
 puts "a = #{side_a} || b = #{side_b} || c = #{side_c}"
 
@@ -15,7 +15,7 @@ h_from_c = calculate_h(side_c)
 puts "h from a = #{side_a} || h from b = #{side_b} || h from c = #{side_c}"
 
 def calculate_m(side_a, side_b, side_c)
-  m_a = 0.5 * Math.sqrt(2 * side_b**2 + (2 * side_c**2) - side_a**2)
+  m_a = 0.5 * Math.cbrt(2 * side_b**2 + (2 * side_c**2) - side_a**2)
 end
 
 median_of_a = calculate_m(side_a, side_b, side_c)
@@ -24,4 +24,13 @@ median_of_c = calculate_m(side_c, side_b, side_a)
 
 puts "m of a = #{median_of_a} || m of b = #{median_of_b} || m of c = #{median_of_c}"
 
+def calculate_l (a, b, c)
+  sqrt = Math.sqrt((a * b) * ( a + b + c ) * ( a + b - c ) )
+  sum = a + b
+  l = sqrt / sum
+end
 
+bisector_a = calculate_l(side_a, side_b, side_c)
+bisector_b = calculate_l(side_b, side_c, side_a)
+bisector_c = calculate_l(side_c, side_b, side_a)
+puts "bisector of a = #{bisector_a} || bisector of b = #{bisector_b} || bisector_of c = #{bisector_c}"
