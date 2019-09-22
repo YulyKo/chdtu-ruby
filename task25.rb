fd80f7ae501bdab4a765f4ccea07e17f0dfb81dc
YulyKo <juliakotenko.forstudy@gmail.com>
2019-09-22 16:49:14 +0300

    task 25 was done

--- task25.rb
+++ task25.rb
@@ -1,1 +1,12 @@
-
+x1 = 789
+y1 = 676
+x2 = 4567
+y2 = 3656
+x3 = 56
+y3 = 32
+def calculatingD (x1, x2, y1, y2)
+    d = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+end    
+s = 0.5 * ((x1 * y2 + (x2 * y3) + (x3 * y1)) - (y1 * x2 + (y2 * x3) + (y3 * x1)))
+p = calculatingD(x1, x2, y1, y2) + calculatingD(x2, x3, y2, y3) + calculatingD(x1, x3, y1, y3)
+puts "S = #{s}\nP = #{p}"
