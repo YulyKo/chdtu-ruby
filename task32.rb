9ddb9ddaafaa1c0f25d4edc9eac60bcdc9e3f2b9
YulyKo <juliakotenko.forstudy@gmail.com>
2019-09-24 14:04:36 +0300

    task 32 was done

---
+++ task32.rb
@@ -0,0 +1,42 @@
+a = 2
+
+# task 32.1 to 4 steps
+a_2 = a * a
+a_3 = a_2 * a
+a_10 = a_3 * a_3 * a_3 * a
+puts "a^3 = #{a_3} || a^10 = #{a_10}"
+
+#task 32.2 to 5 steps
+a_2 = a * a
+a_4 = a_2 * a_2
+a_8 = a_4 * a_4
+a_20 = a_8 * a_8 * a_4
+puts "a^4 = #{a_4} || a^20 = #{a_20}"
+
+#task 32.3 to 5 steps
+a_2 = a * a
+a_3 = a_2 * a
+a_5 = a_2 * a_3
+a_13 = a_5 * a_5 * a_3
+puts "a^5 = #{a_5} || a^13 = #{a_13}"
+
+#task 32.4 to 6 steps
+a_2 = a * a
+a_5 = a_2 * a_2 * a
+a_19 = a_5 * a_4
+puts "a^5 = #{a_5} || a^19 = #{a_19}"
+
+#task 32.5 to 6 steps
+a_2 = a * a
+a_5 = a_2 * a_2 * a
+a_7 = a_5 * a_2
+a_17 = a_5 * a_5 * a_7
+puts "a^2 = #{a_2} || a^5 = #{a_5} || a^17 = #{a_17}"
+
+#task 32.5 to 6 steps
+a_2 = a * a
+a_4 = a_2 * a_2
+a_8 = a_4 * a_4
+a_12 = a_8 * a_4
+a_28 = a_12 * a_12 * a_4
+puts "a^4 = #{a_4} || a^12 = #{a_12} || a^28 = #{a_28}"
