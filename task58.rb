c3b0e9c09f3b6ec66e20127399bd4ce4963b8bae
YulyKo <juliakotenko.forstudy@gmail.com>
2019-10-12 10:24:43 +0300

    task 58 was done

---
+++ task58.rb
@@ -0,0 +1,27 @@
+def task_a (a)
+  if a < 0
+    y = -a
+  elsif a > 0
+    y = -(a**2)
+  elsif a == 0
+    y = 0
+  end
+end
+
+def task_b (a)
+  if a < -1
+    y = 1 / a**2
+  elsif -1 >= a <= 2
+    y = a**2
+  elsif a > 2
+    y = 4
+  end
+end
+
+def task_c (a)
+  if a < -1
+    y = -x
+  elsif -1 > a < 0
+
+  end
+end
