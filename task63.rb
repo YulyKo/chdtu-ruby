a37d4d0d2abd2005fd4ebe454d0696d78a29c05b
YulyKo <juliakotenko.forstudy@gmail.com>
2019-10-12 14:06:27 +0300

    task 63 was done

---
+++ task63.rb
@@ -0,0 +1,10 @@
+def task_63 (a = 6.44, b = 6)
+    r_number = 5.0
+    s_number = 1.0
+    if (a / b)%s_number == 0.0 or (a / b)%r_number == 0.0
+        puts "it posible"
+    else
+        puts "no way"
+    end
+end
+task_63 6,4
