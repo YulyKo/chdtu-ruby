743c384e27f92962509c06d7c7221c1fdc671ed8
YulyKo <juliakotenko.forstudy@gmail.com>
2019-10-22 18:31:29 +0300

    task 207 was done

---
+++ task207.rb
@@ -0,0 +1,9 @@
+n = 32555054503805840503304095050850852000000
+n.to_s
+result_array_of_strings = ""
+for value in 0..n.size do
+    if n[value] != 5 or 0
+        result_array_of_strings + n[value]
+    end
+end
+puts result_array_of_strings
